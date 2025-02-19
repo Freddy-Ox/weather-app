101c8019c72a651c21a248a33abe192377e8f0df
Frederik Okslund <frederik.os@hotmail.com>
2025-02-19 11:51:20 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import { API_KEY } from "./API/api.ts";
+import "./App.css";
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <></>;
+}
+
+export default App;
